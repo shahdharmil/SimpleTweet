a454e2ad0b743d891a537bd9d29ce651b4f100f4
dharmil.ds <dharmil.ds@somaiya.edu>
2022-02-14 10:14:59 -0800

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
@@ -0,0 +1,73 @@
+package com.codepath.apps.restclienttemplate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.codepath.apps.restclienttemplate.models.Tweet
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+import org.json.JSONArray
+import org.json.JSONException
+
+class TimelineActivity : AppCompatActivity() {
+
+    lateinit var client: TwitterClient
+
+    lateinit var rvTweets: RecyclerView
+
+    lateinit var adapter: TweetsAdapter
+
+    val tweets = ArrayList<Tweet>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timeline)
+
+        client = TwitterApplication.getRestClient(this)
+
+        rvTweets = findViewById(R.id.rvTweets)
+        adapter = TweetsAdapter(tweets)
+
+        rvTweets.layoutManager = LinearLayoutManager(this )
+        rvTweets
+
+        populateHomeTimeline()
+
+    }
+
+    fun populateHomeTimeline() {
+        client.getHomeTimeline(object : JsonHttpResponseHandler() {
+
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
+                Log.i(TAG, "onSuccess")
+
+                val jsonArray = json.jsonArray
+
+                try {
+                    val listofnewtweetsRetrieved = Tweet.fromJsonArray(jsonArray)
+                    tweets.addAll(listofnewtweetsRetrieved)
+                    adapter.notifyDataSetChanged()
+                } catch (e: JSONException) {
+                    Log.e(TAG, "JSON Exception $e")
+                }
+
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                response: String?,
+                throwable: Throwable?
+            ) {
+                Log.i(TAG, "onFailure $statusCode")
+            }
+
+        })
+    }
+
+    companion object {
+        val TAG = "TimelineActivity"
+    }
+}
